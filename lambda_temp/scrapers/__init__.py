5ed2b200398cb5f9fa1506dd47b7967542f6ad8e
noaman2540 <g22.noaman.imran@gnkhalsa.edu.in>
2025-11-05 21:04:44 +0530

    Final

--- lambda_temp/scrapers/__init__.py
+++
@@ -1,6 +0,0 @@
-# scrapers/__init__.py
-from .indeed_scraper import IndeedScraper
-from .naukri_scraper import NaukriScraper
-from .linkedin_scraper import LinkedInScraper
-
-__all__ = ['IndeedScraper', 'NaukriScraper', 'LinkedInScraper']
